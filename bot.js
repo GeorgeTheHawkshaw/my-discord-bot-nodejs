fc2f885bdeb2702c30a2e70142f7608ded548194
Harold Foster <itmustbepuppies@gmail.com>
2018-04-04 14:33:23 -0500

    Added a functin that inverts images in the discord

--- bot.js
+++ bot.js
@@ -24,5 +24,55 @@ var command = function(message){
 		console.log(images[i].url)
 		lastimage = images[i].url;
 	}
+
+//Seperate each word in an array
+if (message.content.charAt(0) != "!") return;
+	message.content = message.content.substring(1,message.content.length);
+	var args = message.content.split(" ");
+	var cmd = args[0];
+	var otherWordsThatArentTheFirst = ""
+	for (var i = 1; i < args.length; i++){
+		otherWordsThatArentTheFirst = otherWordsThatArentTheFirst + args[i] + " ";
+	}
+	var otherWordsOfficial = otherWordsThatArentTheFirst;
+	console.log(args);
+
+//Main Switch Case
+	switch(cmd){
+		case "invert":
+			message.channel.send("Inverting...")
+			download.image({
+				irl: lastimage,
+				dest: 'images'
+			}).then(({
+				filename,
+				image
+			}) => {
+				console.log('File saved to', filename)
+
+				//Modify Image
+				Jimp.read(filename, function modify(err,please){
+					if(err) throw err;
+					please.resize(Jimp.AUTHO, 500)
+					.invert()
+					.write(filename); //save
+				});
+
+				function waitaBit(){
+					var embed = new Discord.RichEmbed().attachFile(filename).setDescription("Inverted bois")
+					message.channel.send({
+						embed
+					})
+				}
+				setTimeout(waitaBit, 1500)
+
+			}).catch((err) => {
+				throw err
+			})
+			break;
+		default:
+			message.reply("What's Up?")
+			break;
+	}
 }
 client.login('insert token here');
