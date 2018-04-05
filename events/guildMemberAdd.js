e44f6c782229c3b3060d6f7d4457c6891b6131f9
Harold Foster <itmustbepuppies@gmail.com>
2018-04-05 14:14:15 -0500

    Adds a guildMemberAdd event

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,4 @@
+exports.run = (client, member) => {
+	const defaultChannel = member.guild.channels.find(chan=> chan.permissionsFor(guild.me).has("SEND_MESSAGES");
+		defaultChannel.send("Welcome. Welcome to the server, ${member.user}").catch(console.error);
+}
