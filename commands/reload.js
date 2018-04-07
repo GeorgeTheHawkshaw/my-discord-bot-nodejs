511f0945b090a9bace3037f8e9f8a77be03297a5
Harold Foster <itmustbepuppies@gmail.com>
2018-04-06 22:00:33 -0500

    Fixes typo on reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,4 +1,4 @@
-exports.run = (cliet, message, args, lastimage) => {
+exports.run = (client, message, args, lastimage) => {
 	if(!args || args.size < 1) return message.reply("List the command you want to reload");
 	delete require.cache[require.resolve(`./${args[0]}.js`)];
 	message.reply(`${args[0]} has been reloaded`);
