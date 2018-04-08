b0a08f9b7da7c21f89996eb0ebe7d3eb1111395e
Harold Foster <itmustbepuppies@gmail.com>
2018-04-08 10:57:06 -0500

    Trace command that does edge detection from an image according to a convolution matrix

---
+++ commands/trace.js
@@ -0,0 +1,36 @@
+const download = require('image-downloader')
+const Jimp = require("jimp")
+const Discord = require("discord.js")
+exports.run = (client, message, args, lastimage) => {
+	message.channel.startTyping();
+	download.image({
+		url: lastimage
+		dest: 'images'
+	}).then(({
+		filename,
+		image
+	}) => {
+		console.log('File saved to ' + filename)
+		Jimp.read(filename, function emboss(err, image){
+			if (err) throw err;
+			image.resize(Jimp.AUTO, 500)
+			image.convolute([
+				[0,0,0],
+				[-1,1,0],
+				[0,0,0]
+			]);
+			image.write(filename);
+		});
+
+		function wait(){
+			var embed = new Discord.RichEmbed().attachFile(filename).setDescription("Tada!");
+			message.channel.send({
+				embed
+			})
+			message.channel.stopTyping();
+		}
+		setTimeout(wait, 1500)
+	}).catch((err) => {
+		throw err
+	})
+}
