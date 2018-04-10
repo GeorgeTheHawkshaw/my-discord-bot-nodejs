18fb151387c2c6e73bdbd5b866d644cd4aa052b2
Harold Foster <itmustbepuppies@gmail.com>
2018-04-10 15:50:35 -0500

    Fixes basic typos

--- bot.js
+++ bot.js
@@ -8,45 +8,45 @@ var dispatcher = null
 var startTime, endTime
 var lastimage = ""
 
- fs.readdir("./events/", (err, files) => {
-   if (err) return console.error(err);
-   files.forEach(file => {
-         let eventFunction = require(`./events/${file}`);
-             let eventName = file.split(".")[0];
-                     client.on(eventName, (...args) => eventFunction.run(client, ...args));
-                       });
- });
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
 
 client.on('message', async message => {
-	command(message);
-	console.log(colors.green.underline(message.author.tag))
-	console.log(colors.yellow.bold("\"" + message.content + "\"" ) + " on #" + message.channel.name + "\n")
+  command(message);
+  console.log(colors.green.underline(message.author.tag))
+  console.log(colors.yellow.bold("\"" + message.content + "\"") + " on #" + message.channel.name + "\n")
 });
 
 
-var command = function(message){
+var command = function(message) {
 
-	//Save Uploaded Images to Drive
-	var images = message.attachments.array();
-	for (var i = 0; i <images.length; i++){
-		console.log(images[i].url)
-		lastimage = images[i].url;
-	}
+  //Save Uploaded Images to Drive
+  var images = message.attachments.array();
+  for (var i = 0; i < images.length; i++) {
+    console.log(images[i].url)
+    lastimage = images[i].url;
+  }
 
-if (message.author.bot) return;
-	if(message.content.indexOf(config.prefix) !== 0) return;
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
 
 
-//Seperate each word in an array
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
+  //Seperate each word in an array
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-try{
-	let commandFile = require(`./commands/${command}.js`);
-	commandFile.run(client, message, args, lastimage);
-} catch (err) {
-	console.error(err);
-}
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args, lastimage);
+  } catch (err) {
+    console.error(err);
+  }
 
 }
 client.login(config.token);
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 exports.run = (client, member) => {
-	const defaultChannel = member.guild.channels.find(chan=> chan.permissionsFor(guild.me).has("SEND_MESSAGES"));
-		defaultChannel.send("Welcome. Welcome to the server, ${member.user}").catch(console.error);
+  const defaultChannel = member.guild.channels.find(chan => chan.permissionsFor(guild.me).has("SEND_MESSAGES"));
+  defaultChannel.send(`Welcome. Welcome to the server, ${member.user}`).catch(console.error);
 }
