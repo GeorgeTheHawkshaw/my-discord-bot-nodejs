7f1400ac1ef648486f67334bfe668e432a022a02
Harold Foster <itmustbepuppies@gmail.com>
2018-04-10 15:48:18 -0500

    Fixes .setActivity to .setGame because apparently .setActivity isn't a 'real function'

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 exports.run = (client) => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setActivity(`Mr. Friend is helping ${client.guilds.size} servers!`)
+  client.user.setGame(`Mr. Friend is helping ${client.guilds.size} servers!`)
 };
